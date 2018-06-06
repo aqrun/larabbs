6f5540150ae3046b4a5bd5aecfc0d3b11ef04173
Aqrun <aqrun@oicnp.com>
2018-06-06 17:49:33 +0800

    Add queue for translate slug

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -24,6 +24,10 @@ class TopicObserver
     public function saving(Topic $topic){
         $topic->body = clean($topic->body, 'user_topic_body');
         $topic->excerpt = make_excerpt($topic->body);
+
+    }
+
+    public function saved(Topic $topic){
         if(!$topic->slug) {
             dispatch(new TranslateSlug($topic));
         }
