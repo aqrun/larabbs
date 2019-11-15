3c9a6b28e65142e8f3ea94842b52352f2d3f0a9a
Aqrun <aqruna@gmail.com>
2019-11-15 11:58:42 +0800

    backend content management

--- app/helpers.php
+++ app/helpers.php
@@ -21,3 +21,28 @@ function make_excerpt($value, $length = 200)
     $excerpt = trim(preg_replace('/\r\n|\r|\n+/', ' ', strip_tags($value)));
     return Str::limit($excerpt, $length);
 }
+
+function model_admin_link($title, $model)
+{
+    return model_link($title, $model, 'admin');
+}
+
+function model_link($title, $model, $prefix = '')
+{
+    // 获取模型的复数蛇形命名
+    $model_name = model_plural_name($model);
+    $prefix = $prefix ? "/$prefix/" : '/';
+
+    $url = config('app.url') . $prefix . $model_name . '/' . $model->id;
+    return '<a href="'. $url .'" target="_blank">'. $title .'</a>';
+}
+
+function model_plural_name($model)
+{
+    // 完整类名 如：App\Model\User
+    $full_class_name = get_class($model);
+    $class_name = class_basename($full_class_name);
+    $snake_case_name = Str::snake($class_name);
+    return Str::plural($snake_case_name);
+}
+
