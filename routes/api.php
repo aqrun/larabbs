ef43d9d21020dfcd72a1c4ebcc4e4a795e2522b1
子十 <aqrun@sina.com>
2019-11-21 16:30:08 +0800

    weixin login

--- routes/api.php
+++ routes/api.php
@@ -29,6 +29,12 @@ Route::prefix('v1')
                     ->name('verificationCodes.store');
                 // 用户注册
                 Route::post('users', 'UsersController@store')->name('users.store');
+                //第三方登录 where 限制只匹配weixin  where('social_type', 'weixin|weibo')
+                Route::post('socials/{social_type}/authorizations', 'AuthorizationsController@socialStore')
+                    ->where('social_type', 'weixin')
+                    ->name('socials.authorizations.store');
+
+
             });
 
         Route::middleware('throttle:' . config('api.rate_limits.access'))
